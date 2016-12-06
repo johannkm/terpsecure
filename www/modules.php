c65258c8bf83983e42ec2548609d0c658595e206
Daniel Liscinsky <dmliscinsky@verizon.net>
2016-12-06 09:50:36 -0800

    Merged the changes to index.php into modules.php

--- www/modules.php
+++ www/modules.php
@@ -26,6 +26,68 @@ else{
 ?>
 
 
+
+
+
+<div class="container">
+	<div class="jumbotron">
+			<h1>Stay safe on the web!</h1>
+			<p>Click any module to begin.</p>
+
+			<!-- Page Features -->
+							<div class="row text-center">
+
+									<div class="col-md-3 col-sm-6 hero-feature">
+											<div class="thumbnail">
+
+													<div class="caption">
+															<h3>Password Strength</h3>
+															<br>
+															<p>
+																	<a href="Password Strength.php" class="btn btn-primary">Enter Module</a>
+															</p>
+													</div>
+											</div>
+									</div>
+
+									<div class="col-md-3 col-sm-6 hero-feature">
+											<div class="thumbnail">
+
+													<div class="caption">
+															<h3>Online Advertisements</h3>
+															<br>
+															<p>
+																	<a href="Ad Module/Ad Module Home.php" class="btn btn-primary">Enter Module</a>
+															</p>
+													</div>
+											</div>
+									</div>
+
+									<div class="col-md-3 col-sm-6 hero-feature">
+											<div class="thumbnail">
+
+													<div class="caption">
+															<h3>Secuirty Questions</h3>
+															<br>
+															<p>
+																	<a href="Security Questions Module Part 1.php" class="btn btn-primary">Enter Module</a>
+															</p>
+													</div>
+											</div>
+									</div>
+
+							</div>
+							<!-- /.row -->
+
+</div>
+</div>
+
+
+
+
+
+
+
 <div class="container">
 	<div class="jumbotron">
 		<h1>Stay safe on the web!</h1>
